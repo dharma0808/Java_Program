6f1c9c1f7c20d82d373c9c010278b28bce2a3227
anusha1018 <janardhanaanusha407@gmail.com>
2020-01-28 13:51:46 +0530

    collections

---
+++ CollectionsDemo/src/ctsdemo2/CollectionsDemo.java
@@ -0,0 +1,24 @@
+package ctsdemo2;
+import java.util.ArrayList;
+public class CollectionsDemo {
+	
+	public static void main(String args[])
+	{
+		ArrayList<String> list1= new ArrayList<>();
+		
+		list1.add("Satya");
+		list1.add("Raju");
+		list1.add("Baji");
+		list1.add("Dharma");
+		list1.add("sivaji");
+		
+		ArrayList<String> list2= new ArrayList<>();
+		
+		list2.add("TSR");
+		list1.addAll(list2);
+		System.out.println(list1);
+		
+		list1.remove("Satya");
+		System.out.println(list1);
+	}
+}
